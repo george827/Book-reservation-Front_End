adfabfbdc054049a32a2104c3860a10463cc8514
willy836 <wilsonkimiri836@gmail.com>
2023-05-13 09:34:48 +0300

    Add reservations slice in redux

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import './App.css';
-import React from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import { Routes, Route } from 'react-router-dom';
-import HomePage from './components/HomePage';
-import LoginForm from './components/session/LoginForm';
-import RegistrationForm from './components/session/RegistrationForm';
-import TablesDetails from './components/TableDetails';
-import NavigationPanel from './components/NavigationPanel';
+import "./App.css";
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Routes, Route } from "react-router-dom";
+import Home from "./components/Home";
+import LoginForm from "./components/session/LoginForm";
+import RegistrationForm from "./components/session/RegistrationForm";
+import TablesDetails from "./components/TableDetails";
+import NavigationPanel from "./components/NavigationPanel";
 
 function App() {
   return (
     <main>
       <NavigationPanel />
       <Routes>
-        <Route path="/homepage" element={<HomePage />} />
+        <Route path="/homepage" element={<Home />} />
         <Route path="/" element={<LoginForm />} />
         <Route path="/register" element={<RegistrationForm />} />
-        <Route
-          path="/TableDetails/:tableId"
-          element={<TablesDetails />}
-        />
+        <Route path="/TableDetails/:tableId" element={<TablesDetails />} />
       </Routes>
     </main>
   );
