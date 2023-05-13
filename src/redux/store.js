9ff490ac5e02a7cbfd16bed57520e509ecab89cc
willy836 <wilsonkimiri836@gmail.com>
2023-05-13 09:38:36 +0300

    Rename reservations slice and configure the reducer in store

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,11 @@
-import { configureStore } from '@reduxjs/toolkit';
-import { restaurantTablesReducer } from './tables/restaurantTablesSlice';
+import { configureStore } from "@reduxjs/toolkit";
+import { restaurantTablesReducer } from "./tables/restaurantTablesSlice";
+import { reservationsReducer } from "./reservations/reservationsSlice";
 
 const store = configureStore({
   reducer: {
     restaurantTables: restaurantTablesReducer,
+    reservations: reservationsReducer,
   },
 });
 
