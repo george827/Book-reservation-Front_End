9abc444be2f11699eff63db5560c0b6ec32a9461
willy836 <wilsonkimiri836@gmail.com>
2023-06-03 21:10:01 +0300

    Add more styles to homepage

--- src/components/NavigationPanel.jsx
+++ src/components/NavigationPanel.jsx
@@ -1,22 +1,25 @@
-import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
-import {
-  FaFacebookF, FaTwitter, FaLinkedinIn, FaGithub,
-} from 'react-icons/fa';
-import LogoutButton from './session/LogoutButton';
+import React, { useState } from "react";
+import { Link, NavLink } from "react-router-dom";
+import { FaFacebookF, FaTwitter, FaLinkedinIn, FaGithub } from "react-icons/fa";
+import LogoutButton from "./session/LogoutButton";
 
 const NavigationPanel = () => {
   const [showLinks, setShowLinks] = useState(false);
+
+  const activeStyle = {
+    backgroundColor: "#9acd32",
+    color: "#fff",
+  };
 
   return (
     <section className="nav-section">
       <nav>
         <div className="header">
-          <p className="ms-1">Logo</p>
+          <p className="ms-1">SleekDine</p>
           <button
             type="button"
             data-testid="hamburger-button"
-            className={`hamburger ${showLinks ? 'active' : ''}`}
+            className={`hamburger ${showLinks ? "active" : ""}`}
             onClick={() => setShowLinks(!showLinks)}
           >
             <span className="bar" />
@@ -27,36 +30,65 @@ const NavigationPanel = () => {
 
         {showLinks && (
           <div className="nav-links" data-testid="links-container">
-            <Link to="/homepage" onClick={() => setShowLinks(false)}>
+            <Link
+              to="/homepage"
+              activeClassName="active-link"
+              onClick={() => setShowLinks(false)}
+            >
               Tables
             </Link>
             <Link to="/AddTable">Add Table</Link>
-            <Link to="/DeleteTable">Delete Table</Link>
-            <Link to="/reservation-form">Make Reservation</Link>
-            <Link to="/Myresercvations">My Reservations</Link>
+            <Link to="/DeleteTable" activeClassName="active-link">
+              Delete Table
+            </Link>
+            <Link to="/reservation-form" activeClassName="active-link">
+              Make Reservation
+            </Link>
+            <Link to="/Myresercvations" activeClassName="active-link">
+              My Reservations
+            </Link>
             <LogoutButton />
-
           </div>
         )}
       </nav>
       <div className="sidenav">
-        <h4>Logo</h4>
+        <h4>SleekDine</h4>
         <div className="sidenav-links">
-          <Link to="/homepage" className="sidenav-link">
+          <NavLink
+            to="/homepage"
+            className="sidenav-link"
+            style={({ isActive }) => (isActive ? activeStyle : undefined)}
+          >
             Tables
-          </Link>
-          <Link to="/AddTable" className="sidenav-link">
+          </NavLink>
+          <NavLink
+            to="/AddTable"
+            className="sidenav-link"
+            style={({ isActive }) => (isActive ? activeStyle : undefined)}
+          >
             Add Table
-          </Link>
-          <Link to="/DeleteTable" className="sidenav-link">
+          </NavLink>
+          <NavLink
+            to="/DeleteTable"
+            className="sidenav-link"
+            style={({ isActive }) => (isActive ? activeStyle : undefined)}
+          >
             Delete Table
-          </Link>
-          <Link to="/reservation-form" className="sidenav-link">
+          </NavLink>
+          <NavLink
+            to="/reservation-form"
+            className="sidenav-link"
+            style={({ isActive }) => (isActive ? activeStyle : undefined)}
+          >
             Make Reservation
-          </Link>
-          <Link to="/Myresercvations" className="sidenav-link">
+          </NavLink>
+          <NavLink
+            to="/Myresercvations"
+            className="sidenav-link"
+            style={({ isActive }) => (isActive ? activeStyle : undefined)}
+          >
             My Reservations
-          </Link>
+          </NavLink>
           <LogoutButton className="sidenav-link" />
         </div>
         <ul className="social-links">
